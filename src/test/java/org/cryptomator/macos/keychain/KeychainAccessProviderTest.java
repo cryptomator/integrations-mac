6b229774c5ee23e7b7a4ad310a874e8a16cebc7d
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-03-07 13:46:19 +0100

    fix tests

--- src/test/java/org/cryptomator/macos/keychain/KeychainAccessProviderTest.java
+++ src/test/java/org/cryptomator/macos/keychain/KeychainAccessProviderTest.java
@@ -1,24 +1,18 @@
 package org.cryptomator.macos.keychain;
 
 import org.cryptomator.integrations.keychain.KeychainAccessProvider;
-import org.cryptomator.macos.keychain.MacSystemKeychainAccess;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ServiceLoader;
 
 public class KeychainAccessProviderTest {
 
 	@Test
 	@DisplayName("MacSystemKeychainAccess can be loaded")
 	public void testLoadMacSystemKeychainAccess() {
-		var loadedProviders = ServiceLoader.load(KeychainAccessProvider.class);
-		var provider = loadedProviders.stream()
-				.filter(p -> p.type().equals(MacSystemKeychainAccess.class))
-				.map(ServiceLoader.Provider::get)
-				.findAny();
+		var provider = KeychainAccessProvider.get().findAny();
 		Assertions.assertTrue(provider.isPresent());
+		Assertions.assertInstanceOf(MacSystemKeychainAccess.class, provider.get());
 	}
 
 }
