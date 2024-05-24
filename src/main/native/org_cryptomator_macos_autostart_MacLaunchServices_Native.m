180055e9a5a5b069c58409d2af2884914ab11130
Ralph Plawetzki <ralph@purejava.org>
2024-05-24 19:08:53 +0200

    Migrate legacy login item entry

--- src/main/native/org_cryptomator_macos_autostart_MacLaunchServices_Native.m
+++ src/main/native/org_cryptomator_macos_autostart_MacLaunchServices_Native.m
@@ -10,8 +10,36 @@
 #import <Foundation/Foundation.h>
 #import <ServiceManagement/ServiceManagement.h>
 
+static void migrateSharedFileListLoginItem(void) {
+    LSSharedFileListRef sharedFileList = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
+    NSString *applicationPath = NSBundle.mainBundle.bundlePath;
+   	if (sharedFileList) {
+    	UInt32 seedValue;
+    	NSArray *sharedFileListArray = CFBridgingRelease(LSSharedFileListCopySnapshot(sharedFileList, &seedValue));
+       	for (id sharedFile in sharedFileListArray) {
+       		LSSharedFileListItemRef sharedFileListItem = (__bridge LSSharedFileListItemRef)sharedFile;
+       		CFURLRef applicationPathURL;
+       		if (LSSharedFileListItemResolve(sharedFileListItem, 0, &applicationPathURL, NULL) == noErr) {
+       			NSString *resolvedApplicationPath = [(__bridge NSURL *)applicationPathURL path];
+       			if ([resolvedApplicationPath compare:applicationPath] == NSOrderedSame) {
+           			LSSharedFileListItemRemove(sharedFileList, sharedFileListItem);
+           			NSError* error = nil;
+                    if (![[SMAppService mainAppService] registerAndReturnError: & error]) {
+                        NSLog(@"Failed to add login item: %@", error.localizedDescription);
+                    }
+      			}
+       			CFRelease(applicationPathURL);
+       		}
+       	}
+       	CFRelease(sharedFileList);
+    } else {
+        NSLog(@"Unable to create the shared file list.");
+   	}
+}
+
 JNIEXPORT jboolean JNICALL Java_org_cryptomator_macos_autostart_MacLaunchServices_00024Native_isLoginItemEnabled(JNIEnv *env, jobject thisObj) {
     if (@available(macOS 13, *)) {
+        migrateSharedFileListLoginItem();
         return [[SMAppService mainAppService] status] == SMAppServiceStatusEnabled ? YES : NO;
     } else { // macOS < 13
         LSSharedFileListRef sharedFileList = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
